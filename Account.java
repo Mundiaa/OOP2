2cd8c9f7c7364fd009c2ed581f944e6abab610bb
Mundiaa <167615131+Mundiaa@users.noreply.github.com>
2025-05-18 14:21:31 +0300

    Add files via upload
    Challenge 6

---
+++ Account.java
@@ -0,0 +1,17 @@
+package OOP_pkg;
+
+import javax.sql.DataSource;
+
+abstract public class Account {
+    //Protected fields accessible by subclasses(Admin, User)
+     protected int id;
+     protected String name;
+     //Private field to hold a reference to a Datasource implementation
+     private DataSource myData;
+    // Method to perform an operation (like delete, update, view) by passing in a DataSource strategy
+     public void performOperation(DataSource _myData) throws InterruptedException {
+         myData = _myData; //set the current data operation
+         myData.execute();//Execute the data operation
+
+     }
+ }
